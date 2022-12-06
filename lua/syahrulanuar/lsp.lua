a10a7fd67552e6fdadabaed80508a7aff7ef5ad3
Syahrul Anuar <syahrlanuar95@gmail.com>
2022-12-06 23:19:56 +0800

    update lsp lua and update neovim packages

--- lua/syahrulanuar/lsp.lua
+++ lua/syahrulanuar/lsp.lua
@@ -33,7 +33,7 @@ end
 -- require "lspconfig".cssls.setup { on_attach = on_attach }
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
